52038da890a34ab0f44c31d783e1a1c762c6e824
dplyusnov <dmitry.plyusnov@dataart.com>
2021-11-17 21:00:25 +0200

    detailed logging

--- src/main/groovy/com/nothing/listeners/EmbedPingListener.groovy
+++ src/main/groovy/com/nothing/listeners/EmbedPingListener.groovy
@@ -1,6 +1,7 @@
 package com.nothing.listeners
 
 import com.nothing.annotations.springcomponents.InjectableComponent
+import groovy.util.logging.Slf4j
 import org.javacord.api.entity.message.embed.EmbedBuilder
 import org.javacord.api.event.message.MessageCreateEvent
 
@@ -8,16 +9,22 @@ import static com.nothing.utils.ColorUtils.generateRandomColor
 import static com.nothing.utils.ResourceUtils.getResourceFile
 
 @InjectableComponent
+@Slf4j
 class EmbedPingListener extends KeywordListener {
     @Override
     def process(MessageCreateEvent event, List<String> params) {
-        event.channel.sendMessage(new EmbedBuilder()
+        log.info('Embed ping done')
+        def file = getResourceFile('pics/faceit3.png')
+        log.info('Resource file found')
+        def msgBuilder = new EmbedBuilder()
                 .setAuthor(event.messageAuthor)
                 .setTitle('Embed ping response')
                 .setDescription('Embed pong')
-                .setThumbnail(getResourceFile("pics/faceit3.png"))
+                .setThumbnail(file)
                 .setColor(generateRandomColor())
-        )
+        log.info('Msg builder done')
+        event.channel.sendMessage(msgBuilder)
+        log.info('Msg sent')
     }
 
     @Override
